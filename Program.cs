4bd1e9a588784f5cf1b1c20b0791602daf665ce0
Jose Falanga <jose.falanga@gmail.com>
2020-04-25 17:35:57 +0100

    first version

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SeiyaSagaGen
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+
+    public static class Extension
+    {
+        static Random random = new Random();
+        public static String RandomElement(this List<String> list)
+        {
+            return list[random.Next(0, list.Count - 1)];
+        }
+    }
+}
