008f624ad35d1bec385169ff5e9ecc257751929e
Jose Falanga <jose.falanga@gmail.com>
2020-04-25 14:34:27 -0300

    small refactor

--- Program.cs
+++ Program.cs
@@ -26,9 +26,44 @@ namespace SeiyaSagaGen
     public static class Extension
     {
         static Random random = new Random();
-        public static String RandomElement(this List<String> list)
+        public static String GetRandomElement(this List<String> list)
         {
             return list[random.Next(0, list.Count - 1)];
+        }
+        public static List<String> GetRandomElements(this List<String> list,int amount)
+        {
+            var subset = new List<String>();
+            while(subset.Count != amount)
+            {
+                var randomItem = list[random.Next(0, list.Count - 1)];
+                if (subset.Contains(randomItem))
+                {
+                    subset.Add(randomItem);
+                }
+            }
+            return subset;
+        }
+
+        public static String RemoveRandomElement(this List<String> list)
+        {
+            var item = list.GetRandomElement();
+            list.Remove(item);
+            return item;
+        }
+
+        public static List<String> RemoveRandomElements(this List<String> list, int amount)
+        {
+            var subset = list.GetRandomElements(amount);
+            foreach (var item in subset)
+            {
+                list.Remove(item);
+            }
+            return subset;
+        }
+
+        public static void RemoveIfExists(this List<String> list, String element)
+        {
+            if (list.Contains(element)) list.Remove(element);
         }
     }
 }
